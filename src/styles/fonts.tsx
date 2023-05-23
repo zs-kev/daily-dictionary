62556d8fe249f1800c1cf89376fad18c4dbd1309
Kevin Simon <keviniansimon@gmail.com>
2023-05-23 11:03:54 +0200

    build: added next-themes

---
+++ src/styles/fonts.tsx
@@ -0,0 +1,19 @@
+import localFont from "next/font/local";
+
+const Inconsolata = localFont({
+  src: "./fonts/Inconsolata-VariableFont.ttf",
+});
+
+const Inter = localFont({
+  src: "./fonts/Inter-VariableFont.ttf",
+});
+
+const Lora = localFont({
+  src: "./fonts/Lora-VariableFont.ttf",
+});
+
+const LoraItalic = localFont({
+  src: "./fonts/Lora-Italic-VariableFont.ttf",
+});
+
+export { Inconsolata, Inter, Lora, LoraItalic };
