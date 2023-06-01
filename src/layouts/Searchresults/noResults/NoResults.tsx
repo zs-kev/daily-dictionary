4c64023346c9ba1e54328d9959218f520783c411
Kevin Simon <keviniansimon@gmail.com>
2023-06-01 08:38:55 +0200

    fix: corrected a spelling mistake

--- src/layouts/Searchresults/noResults/NoResults.tsx
+++ src/layouts/Searchresults/noResults/NoResults.tsx
@@ -7,7 +7,7 @@ export default function NoResults() {
       <h1 className={styles.heading}>No Definitions Found</h1>
       <p className={styles.text}>
         Sorry pal, we couldn&apos;t find definitions for the word you were
-        looking for. You can try the search again at later time or head to the
+        looking for. You can try the search again at a later time or head to the
         web instead.
       </p>
     </div>
